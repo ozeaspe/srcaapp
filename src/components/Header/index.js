076d8af6c155bb2ef579c892c10a280cf7600073
Ozeas <ozeaspe@gmail.com>
2024-01-28 08:27:00 -0300

    update header
    ...

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import avatarImg from '../../imagens/avatar.png'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../contexts/auth'
+import { FiLayers, FiUser, FiSettings} from 'react-icons/fi'
 
 export default function Header(){
 
@@ -12,6 +13,21 @@ export default function Header(){
            <div>
             <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt="Foto do Usário"/>
            </div>
+
+           <Link to="/dashboard">
+            <FiLayers size={24} className="icon"/>
+            Anamnese
+            </Link>
+            <Link to="/customers">
+            <FiUser className="icon" size={24}/>
+            Cadastro
+            </Link>
+            <Link to="/profile">
+            <FiSettings className="icon" size={24}/>
+            Perfil
+            </Link>
+           
+        
         </div>
     )
 }
